afbc94d2e46670221db80fdb39a262f9ac9b1480
Kartik <kartik@example.com>
2024-09-27 18:24:54 +0530

    Added 404.html for routing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Header from './Components/Header'; // Ensure you have this line
 import Home from './Components/Home';
-import About from './Components/About';
-import Contact from './Components/Contact';
 import Shop from './Components/Shop';
 import Necklaces from './Components/Necklaces'; // Import Necklaces component
 import Rings from './Components/Rings';
+import About from './Components/About';
+import Contact from './Components/Contact';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
 
@@ -19,11 +19,11 @@ const App = () => {
                 <Header /> {/* Ensure the Header component is rendered */}
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/contact" element={<Contact />} />
                     <Route path="/shop" element={<Shop />} />
                     <Route path="/necklaces" element={<Necklaces />} /> {/* Route for Necklaces */}
                     <Route path="/rings" element={<Rings />} /> {/* Route for Rings */}
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
                 </Routes>
             </div>
         </Router>
